6597bf4b6118434bf757335adbb7c1ba26ac396d
Steven Xie <stevenslxie@gmail.com>
2023-11-28 23:40:12 +0800

    v1.0.55 simplify header

--- src/app/resume-parser/ResumeDisplay.tsx
+++ src/app/resume-parser/ResumeDisplay.tsx
@@ -6,29 +6,17 @@ interface ResumeDisplayProps {
 }
 
 export const ResumeDisplay: React.FC<ResumeDisplayProps> = ({ resumes }) => {
+  const headers = ['Name', 'Email', 'Phone', 'Location', 'Experience', 'Education'];
   return (
     <table className="min-w-full divide-y divide-gray-200 mt-4">
             <thead className="bg-gray-50">
-        <tr>
-            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-            Name
-            </th>
-            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-            Email
-            </th>
-            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-            Phone
-            </th>
-            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-            Location
-            </th>
-            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-            Experience
-            </th>
-            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-            Education
-            </th>
-        </tr>
+            <tr>
+              {headers.map((header) => (
+                <th key={header} className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  {header}
+                </th>
+              ))}
+            </tr>
         </thead>
         <tbody className="bg-white divide-y divide-gray-200">
         {resumes.map((resume, index) => (
