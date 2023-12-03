12708a50f253e90407703342da6af92fa8bdd7ee
Steven Xie <stevenslxie@gmail.com>
2023-12-03 13:10:01 +0800

    v1.1.12 pricing bug

--- src/app/resume-parser/page.tsx
+++ src/app/resume-parser/page.tsx
@@ -198,7 +198,7 @@ export default function ResumeParser() {
             </div>}   
             {resumes.length > displayLimit && 
             <p className="text-gray-500 mt-2 text-xs">
-               <span className="font-semibold">First {LIMITS.DEFAULT_DISPLAY_LIMIT} resumes are shown above for your preview. Pay {resumes.length * 0.1} USD to download all {resumes.length} resumes.</span>
+               <span className="font-semibold">First {LIMITS.DEFAULT_DISPLAY_LIMIT} resumes are shown above for your preview. Pay {Math.max(resumes.length * 0.1, 1)} USD to download all {resumes.length} resumes.</span>
             </p>}
 
             {resumes.length > displayLimit && 
