4f457d5144bad9d16592b6972888136e69b84ced
Steven Xie <stevenslxie@gmail.com>
2023-12-03 08:04:43 +0800

    v1.1.10 try and catch for resume parsing

---
+++ src/constants/index.ts
@@ -0,0 +1,33 @@
+import type { Resume } from "lib/redux/types";
+
+export const LIMITS = {
+    NUM_RESUMES: 5,
+    GPT_TIMEOUT: 35, // a timer inside callGpt, not the maxDuration
+    LINE_LIMIT: 300,
+    PIXEL_WIDTH_LIMIT: 400,
+    DEFAULT_WAITTIME: 25000,
+    DEFAULT_DISPLAY_LIMIT: 3,
+    MAX_INT: 1000000
+  };
+
+  export const DUMMY_RESUME: Resume = {
+    profile: {
+      name: "DummyDummy",
+      email: "",
+      phone: "",
+      url: "",
+      summary: "",
+      location: ""
+    },
+    workExperiences: [],
+    educations: [],
+    projects: [],
+    skills: {
+      featuredSkills: [],
+      descriptions: []
+    },
+    custom: {
+      descriptions: []
+    }
+  };
+
