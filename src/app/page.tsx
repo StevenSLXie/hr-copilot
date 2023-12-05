a9d668b8457bdff204765ae3665cf8b98ef108bb
Steven Xie <stevenslxie@gmail.com>
2023-12-05 23:40:13 +0800

    v1.1.27 home page is resume parser

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,12 +8,12 @@ import ResumeParser from "resume-parser/page";
 export default function Home() {
   return (
     <main className="mx-auto max-w-screen-2xl bg-dot px-8 pb-32 text-gray-900 lg:px-12">
-      <Hero />
+      {/* <Hero />
       <Steps />
       <Features />
       <Testimonials />
-      <QuestionsAndAnswers />
-      {/* <ResumeParser /> */}
+      <QuestionsAndAnswers /> */}
+      <ResumeParser />
     </main>
   );
 }
