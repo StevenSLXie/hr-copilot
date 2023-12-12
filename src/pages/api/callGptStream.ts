4e750e380b551150be3c3aebc3e831b42602e92f
Steven Xie <stevenslxie@gmail.com>
2023-12-12 23:43:50 +0800

    v1.1.51 change prompts

--- src/pages/api/callGptStream.ts
+++ src/pages/api/callGptStream.ts
@@ -14,7 +14,7 @@ const guidelines =
               "4. Stength: highlight 2-3 points that the candidate is strong about and how can the resume be polished to highlight them. give concise examples; " +
               "5. Questions: list 5 questions you would ask if you are the hiring manager in order to deep dive into the candaiate's weakness. Must be specific. each question one line;" +
               "6. Salary: based on the resume, guess the salary range of the candidate given his location. Propose the future salary range if the candidate is hired." + 
-              "the return should be in the format of verdict- text; summary: text; weakness: text; strength: text; questions: text; salary: text; the index and the dot must be included. The overall response should be around 500 words";
+              "the return should be in the format of Verdict: text; Summary: text; Weakness: text; Strength: text; Questions: text; Salary: text; the index and the dot must be included. The overall response should be around 500 words";
  
 export default async function POST(req: Request) {
   // Extract the `prompt` from the body of the request
